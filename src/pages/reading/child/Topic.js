e15127ef2b44f3845da4729595070f342e1ece65
f641753527 <641753527@qq.com>
2019-03-20 18:02:01 +0800

    热门推荐完善

--- src/pages/reading/child/Topic.js
+++ src/pages/reading/child/Topic.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Image } from 'react-native';
 
-import Fetch from '../../../utils/Fetch';
+import utils from '../../../utils';
 
 export default class Topic extends Component{
 
@@ -15,7 +15,15 @@ export default class Topic extends Component{
   render() {
     return (
       <View style={ styles.container }>
-        <Text>话题</Text>
+        <Text style={ styles.title }>{this.props.title}</Text>
+        <View style={ styles.list }>
+          {this.props.list && this.props.list.length ? this.props.list.map(v => 
+            <View style={ [styles.item] } key={v.id}>
+              <Image style={ styles.item_cover_image } source={{uri: v.cover_img}}></Image>
+              <Text numberOfLines={2} style={ styles.item_title }>{v.title}</Text>
+            </View>
+          ) : null}
+        </View>
       </View>
     );
   }
@@ -23,6 +31,33 @@ export default class Topic extends Component{
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1
+    padding: 10,
+    paddingLeft: 0
+  },
+  title: {
+    paddingLeft: 10,
+    marginBottom: 10,
+    fontSize: 16,
+    color: '#252525',
+  },
+  list: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+  },
+  item: {
+    flex: 1,
+    flexBasis: '20%',
+    marginLeft: 10,
+    marginBottom: 20,
+  },
+  item_cover_image: {
+    width: '100%',
+    height: 100
+  },
+  item_title: {
+    // marginTop: 6,
+    fontSize: 10,
+    lineHeight: 20,
+    color: '#7f7f83'
   }
 });
--- src/pages/reading/index.js
+++ src/pages/reading/index.js
@@ -17,17 +17,73 @@ export default class Reading extends Component{
     super(props);
     this.state = {
       is_show: false,
+      hot_list: [],
     }
   }
 
   componentDidMount() {
     this.setState({ is_show: true });
+    this.get_hot_list();
     this.props.navigation.addListener('willFocus', this.willFocus);
   }
 
   willFocus = (payload) => {
     // this.get_list();
     // this.login();
+  }
+
+  get_hot_list = () => {
+    const list = [
+      {
+        id: 1,
+        title: '热门推荐1热门推荐1热门推荐1热门推荐1',
+        cover_img: 'https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=1694255706,456221263&fm=26&gp=0.jpg',
+        link: 'https://www.baidu.com',
+      },
+      {
+        id: 2,
+        title: '热门推荐1',
+        cover_img: 'https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=1694255706,456221263&fm=26&gp=0.jpg',
+        link: 'https://www.baidu.com',
+      },
+      {
+        id: 3,
+        title: '热门推荐1',
+        cover_img: 'https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=1694255706,456221263&fm=26&gp=0.jpg',
+        link: 'https://www.baidu.com',
+      },
+      {
+        id: 4,
+        title: '热门推荐1',
+        cover_img: 'https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=1694255706,456221263&fm=26&gp=0.jpg',
+        link: 'https://www.baidu.com',
+      },
+      {
+        id: 5,
+        title: '热门推荐1',
+        cover_img: 'https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=1694255706,456221263&fm=26&gp=0.jpg',
+        link: 'https://www.baidu.com',
+      },
+      {
+        id: 6,
+        title: '热门推荐1',
+        cover_img: 'https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=1694255706,456221263&fm=26&gp=0.jpg',
+        link: 'https://www.baidu.com',
+      },
+      {
+        id: 7,
+        title: '热门推荐1',
+        cover_img: 'https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=1694255706,456221263&fm=26&gp=0.jpg',
+        link: 'https://www.baidu.com',
+      },
+      {
+        id: 8,
+        title: '热门推荐1',
+        cover_img: 'https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=1694255706,456221263&fm=26&gp=0.jpg',
+        link: 'https://www.baidu.com',
+      },
+    ];
+  this.setState({ hot_list: list });
   }
 
   render() {
@@ -37,12 +93,12 @@ export default class Reading extends Component{
         <Line></Line>
         { 
         this.state.is_show ?
-          <ScrollView>
+          <ScrollView style={ styles.container }>
             <Recommand></Recommand>
             <Line></Line>
-            <Topic></Topic>
+            <Topic title={'热门推荐'} list={this.state.hot_list}></Topic>
             <Category></Category>
-            <Topic></Topic>
+            <Topic title={'清新一刻'} list={this.state.hot_list}></Topic>
             <List></List>
           </ScrollView>
         : null
